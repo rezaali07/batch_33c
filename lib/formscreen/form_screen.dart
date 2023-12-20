dd12c6c6ee744bcfe5095dbd0a6d4fa0d8fcac84
sprajapati123 <sandisprajapati52@gmail.com>
2023-12-20 09:20:41 +0545

    add data to firebase

--- lib/formscreen/form_screen.dart
+++ lib/formscreen/form_screen.dart
@@ -17,46 +17,70 @@ class _FormScreenState extends State<FormScreen> {
 
   final database = FirebaseDatabase.instance;
 
+  final _key = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          TextFormField(
-              controller: fname,
-              decoration: InputDecoration(label: Text("First name"))),
-          TextFormField(
-              controller: lname,
-              decoration: InputDecoration(label: Text("Last name"))),
-          TextFormField(
-              controller: contact,
-              decoration: InputDecoration(label: Text("Contact"))),
-          TextFormField(
-              controller: address,
-              decoration: InputDecoration(label: Text("Address"))),
-          TextFormField(
-              controller: email,
-              decoration: InputDecoration(label: Text("Email"))),
-          ElevatedButton(
-              onPressed: () async {
-                var datas = {
-                  "firstName": fname.text,
-                  "lastName": lname.text,
-                  "contact": contact.text,
-                  "address": address.text,
-                  "email": email.text,
-                };
-                await database.ref().push().set(datas).then((value) {
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text("Success")));
-                }).onError((error, stackTrace) {
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text(error.toString())));
-                });
-              },
-              child: Text("Submit"))
-        ],
+      body: Form(
+        key: _key,
+        child: Column(
+          children: [
+            TextFormField(
+                validator: (value) {
+                  if (value == "") {
+                    return "Enter fname";
+                  }
+                  return null;
+                },
+                controller: fname,
+                decoration: InputDecoration(label: Text("First name"))),
+            TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value == "") {
+                    return "Enter lname";
+                  }
+                  return null;
+                },
+                controller: lname,
+                decoration: InputDecoration(label: Text("Last name"))),
+            TextFormField(
+                controller: contact,
+                decoration: InputDecoration(label: Text("Contact"))),
+            TextFormField(
+                controller: address,
+                decoration: InputDecoration(label: Text("Address"))),
+            TextFormField(
+                controller: email,
+                decoration: InputDecoration(label: Text("Email"))),
+            ElevatedButton(
+                onPressed: () async {
+                  if (_key.currentState!.validate()) {
+                    var datas = {
+                      "firstName": fname.text,
+                      "lastName": lname.text,
+                      "contact": contact.text,
+                      "address": address.text,
+                      "email": email.text,
+                    };
+                    await database
+                        .ref()
+                        .child("users")
+                        .push()
+                        .set(datas)
+                        .then((value) {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text("Success")));
+                    }).onError((error, stackTrace) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text(error.toString())));
+                    });
+                  }
+                },
+                child: Text("Submit"))
+          ],
+        ),
       ),
     );
   }
