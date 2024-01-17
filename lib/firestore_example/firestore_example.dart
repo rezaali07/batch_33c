e5f837a725e37502a12293ce05e92e5c1fb6039d
rezaali2290 <rezaali20ab@gmail.com>
2024-01-17 10:17:07 +0545

    kjddijdio

--- lib/firestore_example/firestore_example.dart
+++ lib/firestore_example/firestore_example.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:batch33c/model/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
